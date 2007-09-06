51c834ad0046e388f61b2dfca2024657e1e58f54
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-06 12:02:51 +0000

    added indicators to TranslatedTextPanel

--- TranslatedTextPanel.cc
+++ TranslatedTextPanel.cc
@@ -1,7 +1,9 @@
 #include "TranslatedTextPanel.h"
 #include "config.h"
+#include "IndicatorWidget.h"
 
-TranslatedTextPanel::TranslatedTextPanel() {
+TranslatedTextPanel::TranslatedTextPanel() :
+	m_fuzzy_ind("Fuzzy"), m_untr_ind("Untranslated"), m_err_ind("Error") {
 	m_title_label.set_text("Transalted text (msgstr): ");
 	m_title_label.set_alignment(Gtk::ALIGN_LEFT, Gtk::ALIGN_CENTER);
 	this->pack_start(*new Gtk::HSeparator(), false, false);
@@ -9,6 +11,10 @@ TranslatedTextPanel::TranslatedTextPanel() {
 	this->pack_start(m_title_box, false, false);
 
 	m_title_box.pack_start(m_title_label, false, false);
+
+	m_title_box.pack_start(m_fuzzy_ind, false, false, 10);
+	m_title_box.pack_start(m_untr_ind, false, false, 10);
+	m_title_box.pack_start(m_err_ind, false, false, 10);
 	
 	this->show_all_children();
 
--- TranslatedTextPanel.h
+++ TranslatedTextPanel.h
@@ -2,6 +2,7 @@
 #define TRANSLATED_TEXT_PANEL_H
 
 #include "TranslateTxtView.h"
+#include "IndicatorWidget.h"
 
 class TranslatedTextPanel : public Gtk::VBox {
 	public:
@@ -10,5 +11,6 @@ class TranslatedTextPanel : public Gtk::VBox {
 		Gtk::HBox m_title_box;
 		Gtk::Label m_title_label;
 		TranslateTxtView m_tr_tv;
+		IndicatorWidget m_fuzzy_ind, m_untr_ind, m_err_ind;
 };
 #endif /* TRANSLATED_TEXT_PANEL_H */
