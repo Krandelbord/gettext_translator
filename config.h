5661b272f358dafbce888da605e0327cb6380426
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-07 23:45:12 +0000

    added pretty print of time in debug function/macro

--- config.h
+++ config.h
@@ -3,7 +3,15 @@
 
 #ifdef DEBUG
 #include <glib.h>
-#  define debug(args...) g_print("\033[6;102m%s\033[1;0m\t", __PRETTY_FUNCTION__); g_print (args)
+#  define debug(args...) { \
+	GTimeVal time; \
+	g_get_current_time(&time); \
+	char buf[BUFSIZ]; \
+	strftime(buf, sizeof(buf), "%H:%M:%S", gmtime(&(time.tv_sec))); \
+	g_print("\033[6;102m%s,%2.0f ", buf, time.tv_usec/1000.0); \
+	g_print("%s\033[1;0m\t", __PRETTY_FUNCTION__); \
+	g_print (args); \
+}
 #else
 #  define debug(args...) ;
 #endif
