d5245d80657715941175950d549461cd49e9e856
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-07 23:19:15 +0000

    added colorful pretty function print

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 #ifdef DEBUG
 #include <glib.h>
-#  define debug(args...) g_print (args)
+#  define debug(args...) g_print("\033[6;102m%s\033[1;0m\t", __PRETTY_FUNCTION__); g_print (args)
 #else
 #  define debug(args...) ;
 #endif
