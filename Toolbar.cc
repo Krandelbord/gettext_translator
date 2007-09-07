dfa7e9185bf061a00cb8109ea9676ea5c4835059
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-07 19:37:40 +0000

    added next/previous buttons to toolbar

--- Toolbar.cc
+++ Toolbar.cc
@@ -16,6 +16,20 @@ Toolbar::Toolbar() {
 	this->append(*new Gtk::ToolButton(Gtk::Stock::FIND_AND_REPLACE));
 	this->append(*new Gtk::SeparatorToolItem());
 
-	this->append(*new Gtk::ToolButton(Gtk::Stock::PROPERTIES));
+	this->append(*new Gtk::ToolButton(Gtk::Stock::GO_BACK));
+	this->append(*new Gtk::ToolButton(Gtk::Stock::GO_FORWARD));
+
+	Gtk::Tooltips *tooltips = new Gtk::Tooltips();
+
+	Gtk::ToolButton *first_tb = new Gtk::ToolButton(Gtk::Stock::GOTO_FIRST);
+	first_tb->set_label("Previous");
+	first_tb->set_tooltip(*tooltips, "Jumps to previous fuzzy or untranslated message");
+
+	Gtk::ToolButton *last_tb = new Gtk::ToolButton(Gtk::Stock::GOTO_LAST);
+	last_tb->set_label("Next");
+	last_tb->set_tooltip(*tooltips, "Jumps to next fuzzy or untranslated message");
+	this->append(*first_tb);
+	this->append(*last_tb);
+
 	this->show_all();
 }
