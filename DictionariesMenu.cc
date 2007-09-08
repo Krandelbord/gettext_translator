cc7402f237c6602227aef9994c4d24cc7bfb21f0
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-08 12:51:28 +0000

    replaces underscores with spaces in dictionaries names

--- DictionariesMenu.cc
+++ DictionariesMenu.cc
@@ -7,7 +7,8 @@ DictionariesMenu::DictionariesMenu() {
 	std::vector<Glib::ustring> dict_list = getDictionaryList();
 	for (std::vector<Glib::ustring>::iterator it = dict_list.begin(); it != dict_list.end(); ++it) {
 		this->items().push_back( 
-				Gtk::Menu_Helpers::MenuElem(*it,
+				Gtk::Menu_Helpers::MenuElem( 
+					replaceAllReturn(*it, "_", " "),
 					sigc::bind<Glib::ustring>(sigc::mem_fun(*this, &DictionariesMenu::onMenuItemSelected), *it) 
 					)
 				);
