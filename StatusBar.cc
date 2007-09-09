3e3394cdcfdb766220681ba9ec87f69232953e74
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-09 12:46:23 +0000

    added all needed methods for status bar

--- StatusBar.cc
+++ StatusBar.cc
@@ -1,12 +1,42 @@
 #include "StatusBar.h"
+#include <sstream>
 
 StatusBar::StatusBar() {
-	m_current_sb.set_has_resize_grip(false);
-	this->pack_start(m_current_sb, true, true);
-
 	m_fuzzy_sb.set_has_resize_grip(false);
 	this->pack_start(m_fuzzy_sb, true, true);
 
 	m_untr_sb.set_has_resize_grip(false);
 	this->pack_start(m_untr_sb, true, true);
+
+	m_total_sb.set_has_resize_grip(false);
+	this->pack_start(m_total_sb, true, true);
+
+	this->setTotal(0);
+	this->setCurrent(0);
+	this->setFuzzy(0);
+	this->setUntranslated(0);
+}
+
+void StatusBar::setTotal(size_t n) {
+	std::ostringstream os;
+	os << "Total" << ": " << n;
+	m_total_sb.push(os.str());
+}
+
+void StatusBar::setCurrent(size_t n) {
+	std::ostringstream os;
+	os << "Current" << ": " << n;
+	this->push(os.str());
+}
+
+void StatusBar::setFuzzy(size_t n) {
+	std::ostringstream os;
+	os << "Fuzzy" << ": " << n;
+	m_fuzzy_sb.push(os.str());
+}
+
+void StatusBar::setUntranslated(size_t n) {
+	std::ostringstream os;
+	os << "Untranslated" << ": " << n;
+	m_untr_sb.push(os.str());
 }
--- StatusBar.h
+++ StatusBar.h
@@ -6,12 +6,13 @@
 class StatusBar : public Gtk::Statusbar {
 	public:
 		StatusBar();
+		void setTotal(size_t n);
 		void setCurrent(size_t n);
 		void setFuzzy(size_t n);
 		void setUntranslated(size_t n);
 
 	private:
-		Gtk::Statusbar m_current_sb;
+		Gtk::Statusbar m_total_sb;
 		Gtk::Statusbar m_fuzzy_sb;
 		Gtk::Statusbar m_untr_sb;
 
