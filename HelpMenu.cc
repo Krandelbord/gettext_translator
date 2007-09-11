972eeaa0a0b1e72f9732d4781ff749069ba0091c
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-11 21:20:20 +0000

    added information about shared libarires in AboutDialog

--- HelpMenu.cc
+++ HelpMenu.cc
@@ -1,4 +1,7 @@
 #include "HelpMenu.h"
+#include <sstream>
+#include <gettext-po.h>
+
 using namespace Gtk;
 
 HelpMenu::HelpMenu(Window *main_win) {
@@ -18,13 +21,25 @@ void HelpMenu::onAbout(Window *main_win) {
 	AboutDialog about;
 	about.set_transient_for(*main_win);
 	about.set_copyright("GNU GPL 2");
-	
+
 	std::list<Glib::ustring> authors;
 	authors.push_back("Author: Emil Nowak <emiml@wp.pl>");
 	about.set_authors(authors);
+	
+	about.set_version("0.01 alpha");
 
 	about.set_translator_credits("");
-	about.set_version("0.01 alpha");
+
+	std::ostringstream os;
+	int v = libgettextpo_version;
+	os << "Used libraries:" << std::endl;
+	os << "libGettext-po version ";
+	os << (v>>16) << "." << (v>>8) << "." << (v-v>>8-v>>16) << std::endl; 
+	
+	os << "Gtkmm version " << GTKMM_MAJOR_VERSION;
+	os << "." << GTKMM_MINOR_VERSION << "." <<  GTKMM_MICRO_VERSION << std::endl;
+
+	about.set_comments(os.str());
 
 	about.run();
 }
