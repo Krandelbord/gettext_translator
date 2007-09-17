3e287c695104188dd220f5080fe8e8db983a3f51
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-17 09:43:14 +0000

    added modifiers definitions as macros

--- config.h
+++ config.h
@@ -19,4 +19,12 @@
 #  define debug(args...) ;
 #endif /* end of DEBUG MACRO */
 
+/* MODIFERIS definitions */
+#define 	MOD__SHIFT   (event->state & GDK_SHIFT_MASK)	
+#define 	MOD__CTRL   (event->state & GDK_CONTROL_MASK)
+#define 	MOD__ALT   (event->state & GDK_MOD1_MASK)
+#define 	MOD__SHIFT_ONLY   ((event->state & GDK_SHIFT_MASK) && !(event->state & GDK_CONTROL_MASK) && !(event->state & GDK_MOD1_MASK))
+#define 	MOD__CTRL_ONLY   (!(event->state & GDK_SHIFT_MASK) && (event->state & GDK_CONTROL_MASK) && !(event->state & GDK_MOD1_MASK))
+#define 	MOD__ALT_ONLY   (!(event->state & GDK_SHIFT_MASK) && !(event->state & GDK_CONTROL_MASK) && (event->state & GDK_MOD1_MASK))
+
 #endif /* CONFIG_H */
