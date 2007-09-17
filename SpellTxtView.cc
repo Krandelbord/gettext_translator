3e468f0f1284bc967b597703f999409c94adf750
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-09-17 20:58:57 +0000

    undo changes with capturing all key_events. We will use accelators

--- SpellTxtView.cc
+++ SpellTxtView.cc
@@ -1,6 +1,5 @@
 #include "SpellTxtView.h"
 #include "config.h"
-#include "keys.h"
 
 SpellTxtView::SpellTxtView() {
 	m_gtk_spell = NULL;
@@ -20,24 +19,4 @@ void SpellTxtView::setLanguage(const Glib::ustring &lang) {
 
 SpellTxtView::~SpellTxtView() {
 	if (m_gtk_spell) gtkspell_detach(m_gtk_spell);
-}
-
-bool SpellTxtView::on_key_press_event(GdkEventKey* event) {
-	if (KEY_COPY_MSGID) {
-		// ctrl + space pressed
-		m_signal_copy_msgid.emit();
-	}
-	if (KEY_TOGGLE_FUZZY) {
-		m_signal_toggle_fuzzy.emit();
-	}
-	Gtk::TextView::on_key_press_event(event);
-	return true;
-}
-
-sigc::signal<void> &SpellTxtView::signal_copy_msgid() {
-	return m_signal_copy_msgid;
-}
-
-sigc::signal<void> &SpellTxtView::signal_toggle_fuzzy() {
-	return m_signal_toggle_fuzzy;
 }
--- SpellTxtView.h
+++ SpellTxtView.h
@@ -11,15 +11,10 @@ class SpellTxtView : public Gtk::TextView {
 	public:
 		SpellTxtView();
 		void setLanguage(const Glib::ustring &lang);
-		sigc::signal<void> &signal_copy_msgid();
-		sigc::signal<void> &signal_toggle_fuzzy();
 
 		virtual ~SpellTxtView();
 
 	private:
-		bool on_key_press_event(GdkEventKey* event);;
 		GtkSpell *m_gtk_spell;
-		sigc::signal<void> m_signal_copy_msgid;
-		sigc::signal<void> m_signal_toggle_fuzzy;
 };
 #endif /* SPELL_TXT_VIEW_H */
