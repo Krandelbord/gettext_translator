a3206cf9fed593aef7f73a627876a51346855981
emil <emil@71639ab1-0c8e-442b-8d5c-40e60f70203c>
2007-11-21 02:26:59 +0000

    added error global variables to handle error messages from gettext-po

--- trunk/ErrorHandlers.cc
+++ trunk/ErrorHandlers.cc
@@ -1,12 +1,27 @@
 #include "ErrorHandlers.h"
 #include <glibmm.h>
 #include "config.h"
+#include <sstream>
+
+extern bool GLOBAL_ERROR_OCCURED;
+extern Glib::ustring global_error_msg;
 
 void xerror_handler (int severity,
 		  po_message_t message,
 		  const char *filename, size_t lineno, size_t column,
 		  int multiline_p, const char *message_text) {
-	debug("\033[1;31mXerror handler \033[1;0m \n");
+
+	GLOBAL_ERROR_OCCURED = true;
+	std::ostringstream ss;
+
+	ss << _("Problem in line") << " " << lineno;
+	ss << " " << _("column") << " " << column << ": " << "\n\n";
+	std::string charset;
+	Glib::get_charset(charset);
+	ss << Glib::convert(message_text, "UTF-8", charset);
+	global_error_msg = ss.str();
+
+	debug("\033[1;31mXerror %d handler \033[1;0m \n", severity);
 	debug("Problem in line %d column %d decsription: %s\n", 
 			lineno, column,
 			Glib::locale_to_utf8(message_text).c_str());
@@ -19,6 +34,21 @@ void xerror2_handler(int severity,
 		   po_message_t message2,
 		   const char *filename2, size_t lineno2, size_t column2,
 		   int multiline_p2, const char *message_text2) {
+
+	GLOBAL_ERROR_OCCURED = true;
+
+	std::string charset;
+	Glib::get_charset(charset);
+
+	std::ostringstream ss;
+	ss << _("Error in two messages\n");
+	ss << _("First in line") << " " << lineno1 << " " << _("column") << " " << column1 << "\n";
+	ss << Glib::convert(message_text1, "UTF-8", charset) << "\n\n";
+	ss << _("Second in line") << " " << lineno2 << " " << _("column") << " " << column2 << "\n";
+	ss << Glib::convert(message_text2, "UTF-8", charset) << "\n\n";
+
+	global_error_msg = ss.str();
+
 	debug("\033[1;31mXerror2 handler \033[1;0m\n");
 	debug("Error int two messages:\n\tFirst in line %d column %d"
 			"\n\tSecond in line %d column %d\n", 
